e1e875374f7232c541f2c9223194016a1570cdfc
Mirko van der Waal <mvdw@airmail.cc>
2017-12-23 15:47:07 +0100

    Advent.Util not as useful for now
    Removed the utility functions as I haven't encounterd them frequently enough
    throughout my days.

--- lib/advent/util.ex
+++ lib/advent/util.ex
@@ -1,31 +1,3 @@
 defmodule Advent.Util do
-  @moduledoc """
-  Recurring common functionality between days.
-  """
-
-  defmodule Circular do
-    @moduledoc false
-
-    @doc """
-    Reimplement &rem/2 behaviour with the exception of 0 being max.
-    """
-    @spec index(integer, integer) :: integer
-    def index(index, max) do
-      if index > max, do: index(index - max, max), else: index
-    end
-
-    @doc """
-    Emulate any amount of numbers constricted to a numeric ceiling.
-    """
-    @spec between(integer, 0, integer) :: []
-    def between(index, 0, max) do
-      []
-    end
-    @spec between(integer, integer, integer) :: [integer, ...]
-    def between(index, until, max) do
-      index + 1..index + until
-      |> Enum.reduce([], fn number, stack -> [index(number, max) | stack] end)
-      |> Enum.reverse
-    end
-  end
+  @moduledoc false
 end
